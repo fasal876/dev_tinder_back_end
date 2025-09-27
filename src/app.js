b269901d16a2d0e75562f46d6403ee853426497d
fasal <muhammedfasalkp710@gmail.com>
2025-09-27 21:56:54 +0530

    playing with multipe handlers

--- src/app.js
+++ src/app.js
@@ -1,28 +1,71 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Testing from server");
-});
-// app.use((req, res) => {
-//   res.send("hello from server");
-// });
-app.use("/hello", (req, res) => {
-  res.send("hello.......................");
-});
-app.use("/hello2", (req, res) => {
-  res.send("hello.......................");
-});
-app.use("/hello3", (req, res) => {
-  res.send("hello.......................");
-});
-app.get("/bla", (req, res) => {
-  console.log("hei");
-  res.send("hi from express");
-});
-app.use("/", (req, res) => {
-  res.send("hello world");
-});
+app.use(
+  "/users1",
+  (req, res, next) => {
+    console.log("req handler 1");
+    next();
+  },
+  (req, res) => {
+    console.log("req handler 2");
+    res.send("request handler 2");
+  }
+);
+
+app.use(
+  "/users2",
+  (req, res, next) => {
+    console.log("req handler 1");
+    res.send("req handler 1");
+    next();
+  },
+  (req, res) => {
+    console.log("req handler 2");
+    res.send("request handler 2");
+  }
+);
+app.use(
+  "/users3",
+  (req, res, next) => {
+    console.log("req handler 1");
+    next();
+    res.send("req handler 1");
+  },
+  (req, res) => {
+    console.log("req handler 2");
+    res.send("request handler 2");
+  }
+);
+app.use(
+  "/users4",
+  (req, res, next) => {
+    console.log("req handler 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("req handler 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("req handler 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("req handler 4");
+    next();
+    res.send("fsjdf");
+  }
+);
+app.use(
+  "/users",
+  (req, res) => {
+    console.log("req handler 1");
+  },
+  (req, res) => {
+    console.log("req handler 1");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running succesfully on port 3000");
